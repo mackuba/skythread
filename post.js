f913cc69ecb3efee70d439807d6eceba7b46c46d
Kuba Suder <jakub.suder@gmail.com>
2023-05-09 19:15:54 +0200

    refactored post div building code

---
+++ post.js
@@ -0,0 +1,142 @@
+class PostComponent {
+  constructor(post, root) {
+    this.post = post;
+    this.root = root;
+  }
+
+  get isRoot() {
+    return this.post === this.root;
+  }
+
+  get linkToPostThread() {
+    return getLocation() + '?q=' + encodeURIComponent(this.post.uri);
+  }
+
+  get linkToAuthor() {
+    return 'https://staging.bsky.app/profile/' + this.post.author.handle;
+  }
+
+  get linkToPost() {
+    return this.linkToAuthor + '/post/' + lastPathComponent(this.post.uri);
+  }
+
+  buildElement() {
+    let div = document.createElement('div');
+    div.className = 'post';
+
+    let header = this.buildPostHeader();
+    div.appendChild(header);
+
+    let p = document.createElement('p');
+    p.innerText = this.post.text;
+    div.appendChild(p);
+
+    let stats = this.buildStatsFooter();
+    div.appendChild(stats);
+
+    for (let reply of this.post.replies) {
+      let component = new PostComponent(reply, this.root);
+      div.appendChild(component.buildElement());
+    }
+
+    if (this.post.replyCount != this.post.replies.length) {
+      let loadMore = this.buildLoadMoreLink()
+      div.appendChild(loadMore);
+    }
+
+    return div;
+  }
+
+  timeFormatForTimestamp() {
+    if (this.isRoot) {
+      return { day: 'numeric', month: 'short', year: 'numeric', hour: 'numeric', minute: 'numeric' };
+    } else if (!sameDay(this.post.createdAt, this.root.createdAt)) {
+      return { day: 'numeric', month: 'short', hour: 'numeric', minute: 'numeric' };
+    } else {
+      return { hour: 'numeric', minute: 'numeric' };
+    }
+  }
+
+  buildPostHeader() {
+    let timeFormat = this.timeFormatForTimestamp();
+    let formattedTime = this.post.createdAt.toLocaleString(window.dateLocale, timeFormat);
+    let isoTime = this.post.createdAt.toISOString();
+
+    let h = document.createElement('h2');
+
+    h.innerHTML = `${this.post.author.displayName} ` +
+      `<a class="handle" href="${this.linkToAuthor}" target="_blank">@${this.post.author.handle}</a> ` +
+      `<span class="separator">&bull;</span> ` +
+      `<a class="time" href="${this.linkToPost}" target="_blank" title="${isoTime}">${formattedTime}</a> `;
+
+    if (this.post.replyCount > 0 && !this.isRoot) {
+      h.innerHTML +=
+        `<span class="separator">&bull;</span> ` +
+        `<a href="${this.linkToPostThread}" class="action" title="Load this subtree">` +
+        `<i class="fa-solid fa-arrows-split-up-and-left fa-rotate-180"></i></a> `;
+    }
+
+    if (this.post.author.avatar) {
+      let avatar = document.createElement('img');
+      avatar.src = this.post.author.avatar;
+      avatar.className = 'avatar';
+      h.prepend(avatar);    
+    } else {
+      let missing = document.createElement('i');
+      missing.className = 'missing fa-regular fa-face-smile fa-2x';
+      h.prepend(missing);
+    }
+
+    return h;
+  }
+
+  buildStatsFooter() {
+    let stats = document.createElement('p');
+    stats.className = 'stats';
+
+    let span = document.createElement('span');
+    let heart = document.createElement('i');
+    heart.className = 'fa-solid fa-heart ' + (this.post.like ? 'liked' : '');
+    heart.addEventListener('click', (e) => this.onHeartClick(heart));
+    span.append(heart);
+    span.append(` ${this.post.likeCount}`);
+    stats.append(span);
+
+    if (this.post.repostCount > 0) {
+      let span = document.createElement('span');
+      span.innerHTML = `<i class="fa-solid fa-retweet"></i> ${this.post.repostCount}`;
+      stats.append(span);
+    }
+
+    return stats;
+  }
+
+  buildLoadMoreLink() {
+    let loadMore = document.createElement('p');
+    let link = document.createElement('a');
+    link.innerText = "Load more replies…";
+    link.href = this.linkToPostThread;
+    loadMore.appendChild(link);
+    return loadMore;
+  }
+
+  onHeartClick(heart) {
+    let api = new BlueskyAPI();
+
+    if (!heart.classList.contains('liked')) {
+      api.likePost(this.post.uri, this.post.cid).then((like) => {
+        this.post.like = like.uri;
+        heart.classList.add('liked');
+      }).catch((error) => {
+        console.log(error);
+      });
+    } else {
+      api.removeLike(this.post.like).then(() => {
+        heart.classList.remove('liked');
+      }).catch((error) => {
+        console.log(error);
+      });
+    }
+  }
+  
+}
--- skythread.js
+++ skythread.js
@@ -31,110 +31,6 @@ function buildPostSubtree(json) {
   return post;
 }
 
-function buildElementForTree(post, root) {
-  let div = document.createElement('div');
-  div.className = 'post';
-
-  let timeFormat;
-
-  if (post === root) {
-    timeFormat = { day: 'numeric', month: 'short', year: 'numeric', hour: 'numeric', minute: 'numeric' };
-  } else if (!sameDay(post.createdAt, root.createdAt)) {
-    timeFormat = { day: 'numeric', month: 'short', hour: 'numeric', minute: 'numeric' };
-  } else {
-    timeFormat = { hour: 'numeric', minute: 'numeric' };
-  }
-
-  let formattedTime = post.createdAt.toLocaleString(window.dateLocale, timeFormat);
-  let isoTime = post.createdAt.toISOString();
-  let profileURL = 'https://staging.bsky.app/profile/' + post.author.handle;
-  let postURL = profileURL + '/post/' + lastPathComponent(post.uri);
-
-  let h = document.createElement('h2');
-  h.innerHTML = `${post.author.displayName} ` +
-    `<a class="handle" href="${profileURL}" target="_blank">@${post.author.handle}</a> ` +
-    `<span class="separator">&bull;</span> ` +
-    `<a class="time" href="${postURL}" target="_blank" title="${isoTime}">${formattedTime}</a> `;
-
-  if (post.replyCount > 0 && post !== root) {
-    let threadURL = getLocation() + '?q=' + encodeURIComponent(post.uri);
-    h.innerHTML +=
-      `<span class="separator">&bull;</span> <a href="${threadURL}" class="action" title="Load this subtree">` +
-      `<i class="fa-solid fa-arrows-split-up-and-left fa-rotate-180"></i></a> `;
-  }
-
-  div.appendChild(h);
-
-  if (post.author.avatar) {
-    let avatar = document.createElement('img');
-    avatar.src = post.author.avatar;
-    avatar.className = 'avatar';
-    h.prepend(avatar);    
-  } else {
-    let missing = document.createElement('i');
-    missing.className = 'missing fa-regular fa-face-smile fa-2x';
-    h.prepend(missing);
-  }
-
-  let p = document.createElement('p');
-  p.innerText = post.text;
-  div.appendChild(p);
-
-  let stats = document.createElement('p');
-  stats.className = 'stats';
-
-  let span = document.createElement('span');
-  let heart = document.createElement('i');
-  heart.className = 'fa-solid fa-heart ' + (post.like ? 'liked' : '');
-  heart.addEventListener('click', (e) => onHeartClick(heart, post));
-  span.append(heart);
-  span.append(` ${post.likeCount}`);
-  stats.append(span);
-
-  if (post.repostCount > 0) {
-    let span = document.createElement('span');
-    span.innerHTML = `<i class="fa-solid fa-retweet"></i> ${post.repostCount}`;
-    stats.append(span);
-  }
-
-  div.appendChild(stats);
-
-  for (let reply of post.replies) {
-    let subdiv = buildElementForTree(reply, root);
-    div.appendChild(subdiv);
-  }
-
-  if (post.replyCount != post.replies.length) {
-    let loadMore = document.createElement('p');
-    let link = document.createElement('a');
-    link.innerText = "Load more replies…";
-    link.href = getLocation() + '?q=' + encodeURIComponent(post.uri);
-    loadMore.appendChild(link);
-    div.appendChild(loadMore);
-  }
-
-  return div;
-}
-
-function onHeartClick(heart, post) {
-  let api = new BlueskyAPI();
-
-  if (!heart.classList.contains('liked')) {
-    api.likePost(post.uri, post.cid).then((like) => {
-      post.like = like.uri;
-      heart.classList.add('liked');
-    }).catch((error) => {
-      console.log(error);
-    });
-  } else {
-    api.removeLike(post.like).then(() => {
-      heart.classList.remove('liked');
-    }).catch((error) => {
-      console.log(error);
-    });
-  }
-}
-
 function buildParentLink(post) {
   let p = document.createElement('p');
   p.className = 'back';
@@ -165,7 +61,7 @@ function loadThread(url) {
       document.body.appendChild(p);
     }
 
-    let list = buildElementForTree(tree, tree);
+    let list = new PostComponent(tree, tree).buildElement();
     hideLoader();
     document.body.appendChild(list);
   }).catch(error => {
