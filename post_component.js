719c97618e1e63f972efcafb08bc946d8f38e677
Kuba Suder <jakub.suder@gmail.com>
2023-07-22 15:59:50 +0200

    extracted image view code to a method

--- post_component.js
+++ post_component.js
@@ -142,63 +142,16 @@ class PostComponent {
       div.className = 'quote-embed'
       div.innerHTML = '<p class="post placeholder">Loading quoted post...</p>';
 
-      this.loadQuotedPost(embed.record.uri, div);
-
-      for (let image of embed.images) {
-        let p = document.createElement('p');
-        p.append('[');
-
-        // TODO: load image
-        let cid = image.image.ref['$link'];
-        let a = document.createElement('a');
-        a.innerText = "Image"
-        a.href = `https://bsky.social/xrpc/com.atproto.sync.getBlob?did=${this.post.author.did}&cid=${cid}`;
-        p.append(a);
-        p.append('] ');
-        wrapper.append(p);        
-
-        if (image.alt) {
-          let details = document.createElement('details');
-          let summary = document.createElement('summary');
-          summary.innerText = 'Show alt';
-          details.appendChild(summary);
-          details.append(image.alt);
-          details.className = 'image-alt';
-          wrapper.appendChild(details);
-        }
-      }
-
+      this.addImagesFromEmbed(embed, wrapper);
       wrapper.appendChild(div);
+
+      this.loadQuotedPost(embed.record.uri, div);
 
       return wrapper;
 
     case ImageEmbed:
       wrapper = document.createElement('div');
-
-      for (let image of embed.images) {
-        let p = document.createElement('p');
-        p.append('[');
-
-        // TODO: load image
-        let cid = image.image.ref['$link'];
-        let a = document.createElement('a');
-        a.innerText = "Image";
-        a.href = `https://bsky.social/xrpc/com.atproto.sync.getBlob?did=${this.post.author.did}&cid=${cid}`;
-        p.append(a);
-        p.append('] ');
-        wrapper.append(p);        
-
-        if (image.alt) {
-          let details = document.createElement('details');
-          let summary = document.createElement('summary');
-          summary.innerText = 'Show alt';
-          details.appendChild(summary);
-          details.append(image.alt);
-          details.className = 'image-alt';
-          wrapper.appendChild(details);
-        }
-      }
-
+      this.addImagesFromEmbed(embed, wrapper);
       return wrapper;
 
     case LinkEmbed:
@@ -217,6 +170,32 @@ class PostComponent {
       p = document.createElement('p');
       p.innerText = `[${embed.type}]`;
       return p;
+    }
+  }
+
+  addImagesFromEmbed(embed, wrapper) {
+    for (let image of embed.images) {
+      let p = document.createElement('p');
+      p.append('[');
+
+      // TODO: load image
+      let cid = image.image.ref['$link'];
+      let a = document.createElement('a');
+      a.innerText = "Image";
+      a.href = `https://bsky.social/xrpc/com.atproto.sync.getBlob?did=${this.post.author.did}&cid=${cid}`;
+      p.append(a);
+      p.append('] ');
+      wrapper.append(p);        
+
+      if (image.alt) {
+        let details = document.createElement('details');
+        let summary = document.createElement('summary');
+        summary.innerText = 'Show alt';
+        details.appendChild(summary);
+        details.append(image.alt);
+        details.className = 'image-alt';
+        wrapper.appendChild(details);
+      }
     }
   }
 
