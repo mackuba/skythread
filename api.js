20d0620d6dd60c6d819a2dd1eb74ce9dca1fa532
Kuba Suder <jakub.suder@gmail.com>
2023-10-07 17:37:55 +0200

    extracted a more general Minisky from BlueskyAPI

--- api.js
+++ api.js
@@ -1,11 +1,3 @@
-class APIError extends Error {
-  constructor(code, json) {
-    super("APIError status " + code + "\n\n" + JSON.stringify(json));
-    this.code = code;
-    this.json = json;
-  }
-}
-
 class URLError extends Error {
   constructor(message) {
     super(message);
@@ -41,127 +33,12 @@ class HandleCache {
   }
 }
 
-class BlueskyAPI {
-  #accessToken;
-  #refreshToken;
-  #userDID;
-
+class BlueskyAPI extends Minisky {
   constructor(host = 'bsky.social', useAuthentication = true) {
-    this.host = host;
-    this.useAuthentication = useAuthentication;
-
-    if (useAuthentication) {
-      this.#accessToken = localStorage.getItem('accessToken');
-      this.#refreshToken = localStorage.getItem('refreshToken');
-      this.#userDID = localStorage.getItem('userDID');      
-    }
+    super(host, useAuthentication);
 
     this.handleCache = new HandleCache();
     this.profiles = {};
-  }
-
-  get isLoggedIn() {
-    return !!(this.#accessToken && this.#refreshToken && this.#userDID);
-  }
-
-  async getRequest(method, params) {
-    let url = `https://${this.host}/xrpc/${method}`;
-
-    if (params) {
-      url += '?' + Object.entries(params).map((x) => {
-        if (x[1] instanceof Array) {
-          return x[1].map((i) => `${x[0]}=${encodeURIComponent(i)}`).join('&');
-        } else {
-          return `${x[0]}=${encodeURIComponent(x[1])}`;
-        }
-      }).join('&');
-    }
-
-    let headers = this.useAuthentication ? { 'Authorization': `Bearer ${this.#accessToken}` } : {};
-    let response = await fetch(url, { headers: headers });
-    let json = await this.parseResponse(response);
-
-    if (this.useAuthentication && this.isInvalidToken(response, json)) {
-      await this.refreshAccessToken();
-      response = await fetch(url, { headers: { 'Authorization': `Bearer ${this.#accessToken}` }});
-      json = await this.parseResponse(response);
-    }
-
-    if (response.status != 200) {
-      throw new APIError(response.status, json);
-    }
-
-    return json;
-  }
-
-  async postRequest(method, data, useRefreshToken, useAuthentication) {
-    let url = `https://${this.host}/xrpc/${method}`;
-    let request = { method: 'POST', headers: {}};
-
-    if (!(useAuthentication === false)) {
-      let token = useRefreshToken ? this.#refreshToken : this.#accessToken;
-      request.headers['Authorization'] = `Bearer ${token}`;
-    }
-
-    if (data) {
-      request.body = JSON.stringify(data);
-      request.headers['Content-Type'] = 'application/json';
-    }
-
-    let response = await fetch(url, request);
-    let json = await this.parseResponse(response);
-
-    if (!useRefreshToken && this.isInvalidToken(response, json)) {
-      await this.refreshAccessToken();
-      request.headers['Authorization'] = `Bearer ${this.#accessToken}`;
-      response = await fetch(url, request);
-      json = await this.parseResponse(response);
-    }
-
-    if (response.status != 200) {
-      throw new APIError(response.status, json);
-    }
-
-    return json;
-  }
-
-  isInvalidToken(response, json) {
-    return (response.status == 400) && json && ['InvalidToken', 'ExpiredToken'].includes(json.error);
-  }
-
-  async parseResponse(response) {
-    let text = await response.text();
-
-    if (text.trim().length > 0) {
-      return JSON.parse(text);
-    } else {
-      return undefined;
-    }
-  }
-
-  async refreshAccessToken() {
-    console.log('Refreshing access token…');
-    let json = await this.postRequest('com.atproto.server.refreshSession', null, true);
-    this.saveTokens(json);
-  }
-
-  async logIn(handle, password) {
-    let json = await this.postRequest('com.atproto.server.createSession', {
-      identifier: handle,
-      password: password
-    }, false, false);
-
-    this.saveTokens(json);
-  }
-
-  saveTokens(json) {
-    this.#accessToken = json['accessJwt'];
-    this.#refreshToken = json['refreshJwt'];
-    this.#userDID = json['did'];
-
-    localStorage.setItem('accessToken', this.#accessToken);
-    localStorage.setItem('refreshToken', this.#refreshToken);
-    localStorage.setItem('userDID', this.#userDID);
   }
 
   cacheProfile(author) {
@@ -255,7 +132,7 @@ class BlueskyAPI {
 
   async likePost(post) {
     return await this.postRequest('com.atproto.repo.createRecord', {
-      repo: this.#userDID,
+      repo: this.userDID,
       collection: 'app.bsky.feed.like',
       record: {
         subject: {
@@ -271,7 +148,7 @@ class BlueskyAPI {
     let { rkey } = atURI(uri);
 
     await this.postRequest('com.atproto.repo.deleteRecord', {
-      repo: this.#userDID,
+      repo: this.userDID,
       collection: 'app.bsky.feed.like',
       rkey: rkey
     });
