bfcd8983db2bd2d755d1c7ffe877c0aa18998d80
Kuba Suder <jakub.suder@gmail.com>
2024-05-10 01:48:16 +0200

    set up Typescript type checking with JSDoc

---
+++ types.d.ts
@@ -0,0 +1,10 @@
+interface Window {
+  unauthed: boolean;
+  root: object;
+  dateLocale: string;
+}
+
+declare var api: BlueskyAPI;
+declare var blue: BlueskyAPI;
+declare var appView: BlueskyAPI;
+
