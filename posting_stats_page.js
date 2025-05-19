d5382fa2a845b0f93c492b5f4237ed05100012fb
Kuba Suder <jakub.suder@gmail.com>
2025-05-19 19:10:12 +0200

    refactor of the posting page code

--- posting_stats_page.js
+++ posting_stats_page.js
@@ -10,6 +10,11 @@ class PostingStatsPage {
   constructor() {
     this.pageElement = $id('posting_stats_page');
 
+    this.rangeInput = $(this.pageElement.querySelector('input[type="range"]'), HTMLInputElement);
+    this.submitButton = $(this.pageElement.querySelector('input[type="submit"]'), HTMLInputElement);
+    this.progressBar = $(this.pageElement.querySelector('input[type=submit] + progress'), HTMLProgressElement);
+    this.table = $(this.pageElement.querySelector('table.scan-result'));
+
     this.setupEvents();
   }
 
@@ -24,16 +29,20 @@ class PostingStatsPage {
       }
     });
 
-    $(this.pageElement.querySelector('input[type="range"]')).addEventListener('input', (e) => {
-      let range = $(e.target, HTMLInputElement);
-      let days = parseInt(range.value, 10);
+    this.rangeInput.addEventListener('input', (e) => {
+      let days = parseInt(this.rangeInput.value, 10);
       this.configurePostingStats({ days });
     });
-    
   }
 
   show() {
     this.pageElement.style.display = 'block';
+  }
+
+  /** @returns {number} */
+
+  selectedDaysRange() {
+    return parseInt(this.rangeInput.value, 10);
   }
 
   /** @param {{ days: number }} args */
@@ -45,138 +54,151 @@ class PostingStatsPage {
     }
   }
 
-  scanPostingStats() {
-    let submit = $(this.pageElement.querySelector('input[type=submit]'), HTMLInputElement);
-    submit.value = 'Cancel';
+  async scanPostingStats() {
+    this.submitButton.value = 'Cancel';
 
-    let range = $(this.pageElement.querySelector('input[type=range]'), HTMLInputElement);
-    let days = parseInt(range.value, 10);
+    let requestedDays = this.selectedDaysRange();
 
-    let progressBar = $(this.pageElement.querySelector('input[type=submit] + progress'), HTMLProgressElement);
-    progressBar.max = days;
-    progressBar.value = 0;
-    progressBar.style.display = 'inline';
+    this.progressBar.max = requestedDays;
+    this.progressBar.value = 0;
+    this.progressBar.style.display = 'inline';
 
-    let table = $(this.pageElement.querySelector('table.scan-result'));
-    table.style.display = 'none';
+    this.table.style.display = 'none';
 
-    let tbody = $(table.querySelector('tbody'));
+    let tbody = $(this.table.querySelector('tbody'));
     tbody.innerHTML = '';
 
-    let now = new Date().getTime();
-    this.scanStartTime = now;
-
-    let minTime = now;
-    let daysBack = 0;
+    let startTime = new Date().getTime();
+    this.scanStartTime = startTime;
 
     let scanInfo = $(this.pageElement.querySelector('.scan-info'));
     scanInfo.style.display = 'none';
 
-    accountAPI.loadTimeline(days, {
+    let items = await accountAPI.loadTimeline(requestedDays, {
       onPageLoad: (data) => {
-        if (this.scanStartTime != now) {
+        if (this.scanStartTime != startTime) {
           return { cancel: true };
         }
 
-        for (let item of data) {
-          let timestamp = item.reason ? item.reason.indexedAt : item.post.record.createdAt;
-          let date = Date.parse(timestamp);
-          minTime = Math.min(minTime, date);
-        }
-
-        daysBack = (now - minTime) / 86400 / 1000;
-        progressBar.value = daysBack;
+        this.updateProgress(data, startTime);
       }
-    }).then(items => {
-      if (this.scanStartTime != now) {
-        return;
+    });
+
+    if (this.scanStartTime != startTime) {
+      return;
+    }
+
+    this.updateResultsTable(items, startTime, requestedDays);
+  }
+
+  /** @param {json[]} dataPage, @param {number} startTime */
+
+  updateProgress(dataPage, startTime) {
+    if (dataPage.length == 0) { return }
+
+    let last = dataPage[dataPage.length - 1];
+    let lastTimestamp = last.reason ? last.reason.indexedAt : last.post.record.createdAt;
+    let lastDate = Date.parse(lastTimestamp);
+
+    let daysBack = (startTime - lastDate) / 86400 / 1000;
+    this.progressBar.value = daysBack;    
+  }
+
+  /** @param {json} a, @param {json} b, @returns {number} */
+
+  sortUserRows(a, b) {
+    let asum = a.own + a.reposts;
+    let bsum = b.own + b.reposts;
+
+    if (asum < bsum) {
+      return 1;
+    } else if (asum > bsum) {
+      return -1;
+    } else {
+      return 0;
+    }
+  }
+
+  /** @param {json[]} items, @param {number} startTime, @param {number} requestedDays */
+
+  updateResultsTable(items, startTime, requestedDays) {
+    let users = {};
+    let total = 0;
+    let allReposts = 0;
+    let allNormalPosts = 0;
+
+    let last = items[items.length - 1];
+    let lastTimestamp = last.reason ? last.reason.indexedAt : last.post.record.createdAt;
+    let lastDate = Date.parse(lastTimestamp);
+    let daysBack = (startTime - lastDate) / 86400 / 1000;
+
+    for (let item of items) {
+      if (item.reply) { continue; }
+
+      let user = item.reason ? item.reason.by : item.post.author;
+      let handle = user.handle;
+      users[handle] = users[handle] ?? { handle: handle, own: 0, reposts: 0, avatar: user.avatar };
+      total += 1;
+
+      if (item.reason) {
+        users[handle].reposts += 1;
+        allReposts += 1;
+      } else {
+        users[handle].own += 1;
+        allNormalPosts += 1;
       }
+    }
 
-      let users = {};
-      let total = 0;
-      let allReposts = 0;
-      let allNormalPosts = 0;
+    let tbody = $(this.table.querySelector('tbody'));
+    let tr = $tag('tr.total');
 
-      for (let item of items) {
-        if (item.reply) { continue; }
+    tr.append(
+      $tag('td.no', { text: '' }),
+      $tag('td.handle', { text: 'Total:' }),
+      $tag('td', { text: (total / daysBack).toFixed(1) }),
+      $tag('td', { text: (allNormalPosts / daysBack).toFixed(1) }),
+      $tag('td', { text: (allReposts / daysBack).toFixed(1) }),
+      $tag('td.percent', { text: '' })
+    );
 
-        let user = item.reason ? item.reason.by : item.post.author;
-        let handle = user.handle;
-        users[handle] = users[handle] ?? { handle: handle, own: 0, reposts: 0, avatar: user.avatar };
-        total += 1;
+    tbody.append(tr);
 
-        if (item.reason) {
-          users[handle].reposts += 1;
-          allReposts += 1;
-        } else {
-          users[handle].own += 1;
-          allNormalPosts += 1;
-        }
-      }
+    let sorted = Object.values(users).sort(this.sortUserRows);
 
-      let tr = $tag('tr.total');
+    for (let i = 0; i < sorted.length; i++) {
+      let user = sorted[i];
+      let tr = $tag('tr');
 
       tr.append(
-        $tag('td.no', { text: '' }),
-        $tag('td.handle', { text: 'Total:' }),
-        $tag('td', { text: (total / daysBack).toFixed(1) }),
-        $tag('td', { text: (allNormalPosts / daysBack).toFixed(1) }),
-        $tag('td', { text: (allReposts / daysBack).toFixed(1) }),
-        $tag('td.percent', { text: '' })
+        $tag('td.no', { text: i + 1 }),
+        $tag('td.handle', {
+          html: `<img class="avatar" src="${user.avatar}"> ` + 
+                `<a href="https://bsky.app/profile/${user.handle}" target="_blank">${user.handle}</a>`
+        }),
+        $tag('td', { text: ((user.own + user.reposts) / daysBack).toFixed(1) }),
+        $tag('td', { text: user.own > 0 ? (user.own / daysBack).toFixed(1) : '–' }),
+        $tag('td', { text: user.reposts > 0 ? (user.reposts / daysBack).toFixed(1) : '–' }),
+        $tag('td.percent', { text: ((user.own + user.reposts) * 100 / total).toFixed(1) + '%' })
       );
 
       tbody.append(tr);
+    }
 
-      let sorted = Object.values(users).sort((a, b) => {
-        let asum = a.own + a.reposts;
-        let bsum = b.own + b.reposts;
+    if (Math.ceil(daysBack) < requestedDays) {
+      let scanInfo = $(this.pageElement.querySelector('.scan-info'));
+      scanInfo.innerText = `🕓 Showing data from ${Math.round(daysBack)} days (your timeline only goes that far):`;
+      scanInfo.style.display = 'block';
+    }
 
-        if (asum < bsum) {
-          return 1;
-        } else if (asum > bsum) {
-          return -1;
-        } else {
-          return 0;
-        }
-      });
-
-      for (let i = 0; i < sorted.length; i++) {
-        let user = sorted[i];
-        let tr = $tag('tr');
-
-        tr.append(
-          $tag('td.no', { text: i + 1 }),
-          $tag('td.handle', {
-            html: `<img class="avatar" src="${user.avatar}"> ` + 
-                  `<a href="https://bsky.app/profile/${user.handle}" target="_blank">${user.handle}</a>`
-          }),
-          $tag('td', { text: ((user.own + user.reposts) / daysBack).toFixed(1) }),
-          $tag('td', { text: user.own > 0 ? (user.own / daysBack).toFixed(1) : '–' }),
-          $tag('td', { text: user.reposts > 0 ? (user.reposts / daysBack).toFixed(1) : '–' }),
-          $tag('td.percent', { text: ((user.own + user.reposts) * 100 / total).toFixed(1) + '%' })
-        );
-
-        tbody.append(tr);
-      }
-
-      if (Math.ceil(daysBack) < days) {
-        scanInfo.innerText = `🕓 Showing data from ${Math.round(daysBack)} days (your timeline only goes that far):`;
-        scanInfo.style.display = 'block';
-      }
-
-      table.style.display = 'table';
-      submit.value = 'Start scan';
-      progressBar.style.display = 'none';
-      this.scanStartTime = undefined;
-    });
+    this.table.style.display = 'table';
+    this.submitButton.value = 'Start scan';
+    this.progressBar.style.display = 'none';
+    this.scanStartTime = undefined;
   }
 
   stopScan() {
-    let submit = $(this.pageElement.querySelector('input[type=submit]'), HTMLInputElement);
-    submit.value = 'Start scan';
+    this.submitButton.value = 'Start scan';
     this.scanStartTime = undefined;
-
-    let progressBar = $(this.pageElement.querySelector('input[type=submit] + progress'), HTMLProgressElement);
-    progressBar.style.display = 'none';
+    this.progressBar.style.display = 'none';
   }
 }
