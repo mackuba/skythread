27c7f92c255e9c647f7267282155ee5d5462df0c
Kuba Suder <jakub.suder@gmail.com>
2025-05-30 22:15:18 +0200

    display invalid handle differently

--- post_component.js
+++ post_component.js
@@ -211,11 +211,12 @@ class PostComponent {
     h.innerHTML = `${escapeHTML(this.authorName)} `;
 
     if (this.post.isFediPost) {
-      let handle = this.post.authorFediHandle;
-      h.innerHTML += `<a class="handle" href="${this.linkToAuthor}" target="_blank">@${handle}</a> ` +
+      let handle = `@${this.post.authorFediHandle}`;
+      h.innerHTML += `<a class="handle" href="${this.linkToAuthor}" target="_blank">${handle}</a> ` +
         `<img src="icons/mastodon.svg" class="mastodon"> `;
     } else {
-      h.innerHTML += `<a class="handle" href="${this.linkToAuthor}" target="_blank">@${this.post.author.handle}</a> `;
+      let handle = (this.post.author.handle != 'handle.invalid') ? `@${this.post.author.handle}` : '[invalid handle]';
+      h.innerHTML += `<a class="handle" href="${this.linkToAuthor}" target="_blank">${handle}</a> `;
     }
 
     h.innerHTML += `<span class="separator">&bull;</span> ` +
