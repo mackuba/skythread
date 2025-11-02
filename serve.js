32f1dec0bbe44da310ec1cb424d6766a4debfced
Kuba Suder <jakub.suder@gmail.com>
2025-11-02 04:22:52 +0200

    added some logs in the console

--- serve.js
+++ serve.js
@@ -4,7 +4,13 @@ Bun.serve({
   routes: {
     '/': {
       async GET(req) {
-        await runBuild(true);
+        let start = Bun.nanoseconds();
+        let result = await runBuild(true);
+        let timeSpent = Bun.nanoseconds() - start;
+        let ms = Math.floor(timeSpent / 100_000) / 10;
+        
+        console.log(`[${new Date().toUTCString()}] Built ${result.outputs.find(x => x.kind == 'entry-point').path} in ${ms} ms`);
+
         return new Response(Bun.file("./index.html"));
       }
     }
