deb5194e80f528397c3ec580753ee4b7f13cf738
Kuba Suder <jakub.suder@gmail.com>
2025-11-24 04:23:15 +0200

    fixed blocked thread root parent

--- src/components/posts/ThreadRootParent.svelte
+++ src/components/posts/ThreadRootParent.svelte
@@ -1,24 +1,22 @@
 <script>
   import { Post, BlockedPost, MissingPost } from '../../models/posts.js';
-  import PostComponent from './PostComponent.svelte';
   import { linkToPostThread } from '../../router.js';
+  import { setContext } from 'svelte';
+  import BlockedPostView from './BlockedPostView.svelte';
 
   let { post } = $props();
+
+  setContext('post', { post: post, context: 'parent' });
 </script>
 
 {#if post instanceof BlockedPost}
-  <PostComponent {post} context="parent" replaceClass="back" />
-
-  <!-- TODO
-    let span = $(element.querySelector('p.blocked-header span'));
-    span.innerText = 'Parent post blocked';
-    -->
-
+  <div class="back">
+    <BlockedPostView reason="Parent post blocked" />
+  </div>
 {:else if post instanceof MissingPost}
   <p class="back">
     <i class="fa-solid fa-ban"></i> parent post has been deleted
   </p>
-
 {:else}
   <p class="back">
     <i class="fa-solid fa-reply"></i>
