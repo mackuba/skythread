cb4ea26c3ed69293229d6e5771792a81d09c5865
Kuba Suder <jakub.suder@gmail.com>
2025-11-27 23:28:34 +0200

    extracted some utils to separate files

--- src/utils.ts
+++ src/utils.ts
@@ -1,34 +1,8 @@
-import DOMPurify from 'dompurify';
-import { URLError } from './api/api.js';
-
-export class AtURI {
-  repo: string;
-  collection: string;
-  rkey: string;
-
-  constructor(uri: string) {
-    if (!uri.startsWith('at://')) {
-      throw new URLError(`Not an at:// URI: ${uri}`);
-    }
-
-    let parts = uri.split('/');
-
-    if (parts.length != 5) {
-      throw new URLError(`Invalid at:// URI: ${uri}`);
-    }
-
-    this.repo = parts[2];
-    this.collection = parts[3];
-    this.rkey = parts[4];
-  }
-}
+export * from './utils/at_uri.js';
+export * from './utils/text.js';
 
 export function $id<T>(name: string, type?: new (...args: any[]) => T): T {
   return document.getElementById(name) as T;
-}
-
-export function atURI(uri: string): AtURI {
-  return new AtURI(uri);
 }
 
 export function castToInt(value: any): number | null | undefined {
@@ -54,28 +28,6 @@ export function isValidURL(url: string): boolean {
   }
 }
 
-export function numberOfDays(days: number): string {
-  return (days == 1) ? '1 day' : `${days} days`;
-}
-
-export function sanitizeHTML(html: string): string {
-  return DOMPurify.sanitize(html, {
-    ALLOWED_TAGS: [
-      'a', 'b', 'blockquote', 'br', 'code', 'dd', 'del', 'div', 'dl', 'dt', 'em', 'font',
-      'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'i', 'li', 'ol', 'p', 'q', 'pre', 's', 'span', 'strong',
-      'sub', 'sup', 'u', 'wbr', '#text'
-    ],
-    ALLOWED_ATTR: [
-      'align', 'alt', 'class', 'clear', 'color', 'dir', 'href', 'lang', 'rel', 'title', 'translate'
-    ]
-  });
-}
-
-export function showError(error: Error) {
-  console.log(error);
-  alert(error);
-}
-
 export function sameDay(date1: Date, date2: Date): boolean {
   return (
     date1.getDate() == date2.getDate() &&
@@ -84,10 +36,7 @@ export function sameDay(date1: Date, date2: Date): boolean {
   );
 }
 
-export function truncateText(text: string, maxLen: number): string {
-  if (text.length <= maxLen) {
-    return text;
-  } else {
-    return text.slice(0, maxLen - 1) + '…';
-  }
+export function showError(error: Error) {
+  console.log(error);
+  alert(error);
 }
---
+++ src/utils/text.ts
@@ -0,0 +1,26 @@
+import DOMPurify from 'dompurify';
+
+export function numberOfDays(days: number): string {
+  return (days == 1) ? '1 day' : `${days} days`;
+}
+
+export function sanitizeHTML(html: string): string {
+  return DOMPurify.sanitize(html, {
+    ALLOWED_TAGS: [
+      'a', 'b', 'blockquote', 'br', 'code', 'dd', 'del', 'div', 'dl', 'dt', 'em', 'font',
+      'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'i', 'li', 'ol', 'p', 'q', 'pre', 's', 'span', 'strong',
+      'sub', 'sup', 'u', 'wbr', '#text'
+    ],
+    ALLOWED_ATTR: [
+      'align', 'alt', 'class', 'clear', 'color', 'dir', 'href', 'lang', 'rel', 'title', 'translate'
+    ]
+  });
+}
+
+export function truncateText(text: string, maxLen: number): string {
+  if (text.length <= maxLen) {
+    return text;
+  } else {
+    return text.slice(0, maxLen - 1) + '…';
+  }
+}
