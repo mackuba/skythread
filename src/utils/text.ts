62537ce55d65c78bd8b5b6d4af35c1571a163c4e
Kuba Suder <jakub.suder@gmail.com>
2026-06-16 19:06:11 +0200

    tweaked dompurify allowed tags/attrs list

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -16,12 +16,12 @@ export function pluralize(value: number, word: string, plural?: string) {
 export function sanitizeHTML(html: string): string {
   return DOMPurify.sanitize(html, {
     ALLOWED_TAGS: [
-      'a', 'b', 'blockquote', 'br', 'code', 'dd', 'del', 'div', 'dl', 'dt', 'em', 'font',
-      'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'i', 'li', 'ol', 'p', 'q', 'pre', 's', 'span', 'strong',
-      'sub', 'sup', 'u', 'wbr', '#text'
+      'a', 'b', 'bdi', 'blockquote', 'br', 'cite', 'code', 'dd', 'del', 'div', 'dl', 'dt', 'em',
+      'h1', 'h2', 'h3', 'h4', 'h5', 'hr', 'i', 'li', 'ol', 'p', 'pre', 'q', 'rp', 'rt', 'ruby',
+      's', 'small', 'span', 'strong', 'sub', 'sup', 'u', 'ul', 'wbr'
     ],
     ALLOWED_ATTR: [
-      'align', 'alt', 'class', 'clear', 'color', 'dir', 'href', 'lang', 'rel', 'title', 'translate'
+      'align', 'class', 'dir', 'href', 'lang', 'title', 'translate'
     ]
   });
 }
